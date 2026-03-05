f3c99485e09c581bfa5979d4200eaf101a1ccdf0
luvtomar2005 <luvtomar2005@gmail.com>
2026-03-05 20:49:52 +0530

    Explore get , post and delete

--- DevBridge/src/app.js
+++ DevBridge/src/app.js
@@ -2,18 +2,34 @@ const express = require('express');
 
 const app = express();
 
+// This will only handle the get call..
+app.get('/user' ,  (req , res) => {
+    res.send({firstName : "Luv Tomar"});
+})
+app.post('/user' ,  (req , res) => {
+    res.send("data added sucessfully");
+})
+app.use('/checking', (req,res)=>{
+    res.send("Checking page");
+});
+
+
+app.use('/testing', (req,res)=>{
+    res.send("Server is running fine");
+});
+
+app.use('/', (req,res)=>{
+    res.send("Hello from dashboard");
+});
+
+
+
+
+
+
 app.listen(1000 , () => {
     console.log("Server is successfully running on port 1000");
 });
 
-app.get('/testing', (req,res)=>{
-    res.send("Server is running fine");
-});
 
-app.get('/checking', (req,res)=>{
-    res.send("Checking page");
-});
 
-app.get('/', (req,res)=>{
-    res.send("Hello from dashboard");
-});
