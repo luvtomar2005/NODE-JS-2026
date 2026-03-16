9bcaf2a9b95eccf81ddf69d7ea283e1ef0e309d6
luvtomar2005 <luvtomar2005@gmail.com>
2026-03-16 23:33:52 +0530

    Created send connection request api

--- DevBridge/src/routes/request.js
+++ DevBridge/src/routes/request.js
@@ -1,21 +1,68 @@
 const express = require("express");
+const requestRouter = express.Router();
 
-const requestRouter  = express.Router();
-const { userAuth } = require('../middleware/auth');
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/users");
 
+const { userAuth } = require("../middleware/auth");
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
 
-/* SEND CONNECTION REQUEST */
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status type: " + status });
+      }
+      //Important for checking whether we are sending request to existing user or not..
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(404).json({ message: "User not found!" });
+      }
+      /* THE BELOW LINE IS THE USAGE OF MONGOOSE or OPERATOR FOR CHECKING WHETHER
+     THE DUPLICATE REQUEST HAS BEEN SENT OR NOT */
 
-    console.log("Sending connection request");
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .send({ message: "Connection Request Already Exists!!" });
+      }
 
-    res.send(user.firstName + " sent the connection request!");
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
-});
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
 
+      const data = await connectionRequest.save();
+
+      // const emailRes = await sendEmail.run(
+      //   "A new friend request from " + req.user.firstName,
+      //   req.user.firstName + " is " + status + " in " + toUser.firstName
+      // );
+      // console.log(emailRes);
+
+      res.json({
+        message:
+          req.user.firstName + " is " + status + " in " + toUser.firstName,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("ERROR: " + err.message);
+    }
+  },
+);
 
 module.exports = requestRouter;
