6358a46394052ef023b91516f76d055905f9f204
luvtomar2005 <luvtomar2005@gmail.com>
2026-03-16 23:33:24 +0530

    typo error fixed

--- DevBridge/src/middleware/auth.js
+++ DevBridge/src/middleware/auth.js
@@ -18,7 +18,7 @@ const userAuth = async (req , res , next) => {
        
     }
     catch(err){
-        res.status(400).send("ERROR" , err.message);
+        res.status(400).send("ERROR"  +  err.message);
     }
 }
 
