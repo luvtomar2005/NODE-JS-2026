36d26a9fec347c0a2524188632ac282aef4e561d
luvtomar2005 <luvtomar2005@gmail.com>
2026-03-23 16:30:48 +0530

    Added the feed api and the pagination logic into the project

--- DevBridge/src/routes/user.js
+++ DevBridge/src/routes/user.js
@@ -26,7 +26,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   }
 });
 
-module.exports = userRouter;
+
 
 // Creating api for getting all the connections
 userRouter.get("/user/connections" , userAuth , async(req  , res) => {
@@ -55,4 +55,49 @@ userRouter.get("/user/connections" , userAuth , async(req  , res) => {
     }
 })
 
+
+// Creating the feed api and pagination into it
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+    
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [
+        { fromUserId: loggedInUser._id },
+        { toUserId: loggedInUser._id },
+      ],
+    }).select("fromUserId toUserId");
+
+    // 🔥 build hidden users set
+    const hiddenUsersFromFeed = new Set();
+
+    connectionRequests.forEach((req) => {
+      hiddenUsersFromFeed.add(req.fromUserId.toString());
+      hiddenUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hiddenUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }) .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+
+    res.json({ data: users });
+
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
+  }
+});
+
+
+
+module.exports = userRouter;
 
