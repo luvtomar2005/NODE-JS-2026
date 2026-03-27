66ce4452304c52dea4be258c333c9617b9583fc0
luvtomar2005 <luvtomar2005@gmail.com>
2026-03-27 09:30:11 +0530

    fix the "/" issue

--- DevBridge/src/routes/auth.js
+++ DevBridge/src/routes/auth.js
@@ -64,9 +64,9 @@ authRouter.post("/login", async (req, res) => {
 
 
 /* LOGOUT API */
-authRouter.post("logout" , async (req , res) => {
+authRouter.post("/logout" , async (req , res) => {
   res.cookie("token" , null , {
-    expires : new Date(Date.now),
+    expires : new Date(Date.now()),
   })
 
   res.send();
